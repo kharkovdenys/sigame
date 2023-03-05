a27217f2830883d9a701a2d33b1ee84ae2110f74
Denys Kharkov <kharkov.denys@lll.kpi.ua>
2023-03-05 14:01:41 +0100

    A couple of improvements

--- src/app/services/socket.service.spec.ts
+++ src/app/services/socket.service.spec.ts
@@ -1,7 +1,7 @@
 import { SocketIoModule } from 'ngx-socket-io';
 
 import { TestBed } from '@angular/core/testing';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import config from './socket.config';
 import { SocketService } from './socket.service';
 
@@ -10,7 +10,7 @@ describe('SocketService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [SocketIoModule.forRoot(config)],
+      imports: [SocketIoModule.forRoot(config), MatSnackBarModule],
       providers: [SocketService]
     });
     service = TestBed.inject(SocketService);
